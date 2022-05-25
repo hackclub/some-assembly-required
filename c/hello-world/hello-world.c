715bcb6c29796b98f8ae74e41fefa25fcc6b080e
Jessica Card <jessicard@mac.com>
2022-05-25 10:41:19 -0400

    Add hello world C program

---
+++ c/hello-world/hello-world.c
@@ -0,0 +1,23 @@
+// Simple C program to display "Hello World"
+
+// We have 2 steps to run our program.
+// 1. generate our executable
+// 2. run our executable
+
+// 1. generate our executable
+// gcc -o hello-world hello-world.c
+
+// 2. run our executable
+// ./hello-world
+
+// Header file for input output functions
+#include <stdio.h>
+
+// main function -
+// where the execution of program begins
+int main() {
+  // prints hello world
+  printf("Hello, world!");
+
+  return 0;
+}
