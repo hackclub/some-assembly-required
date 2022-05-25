f542ec84da58933469c91c32f02004c76435a793
Jessica Card <jessicard@mac.com>
2022-05-25 12:08:32 -0400

    Fix comments

--- c/uppercaser/uppercaser.c
+++ c/uppercaser/uppercaser.c
@@ -2,7 +2,7 @@
 // UPPERCASER
 //
 // Uppercases your command line arguments and prints them out!
-// Written as a working example of a C program to compare to assembly programs
+// Written as a working example of a C program to compare to uppercaser.asm
 // ==================================================================================
 
 // gcc -o uppercaser uppercaser.c && ./uppercaser
@@ -19,8 +19,8 @@ int main(int argc, char **argv) {
   // Uncomment this to print out number of arguments
   // printNumberOfArgs(argc);
 
-  // Our first argument is the program we're running
-  // In this case, that's ./uppercaser.c
+  // Our first argument is the path used to run the program
+  // In this case, that's ./uppercaser
   // We only want to print out additional command line arguments, so let's skip
   // our first argument
   if (argc > 1) {
