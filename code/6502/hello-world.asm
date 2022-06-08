9dfdfd77071c7480ca227974f10cc92cc8f22532
Jessica Card <jessicard@mac.com>
2022-06-08 15:09:05 -0400

    Add comments to 6502 program

--- code/6502/hello-world.asm
+++ code/6502/hello-world.asm
@@ -7,32 +7,91 @@
 ;==========================================================
 
 * = $0801
-
     !byte $0b, $08
-    !byte $E3                     ; BASIC line number:  $E2=2018 $E3=2019 etc
+    !byte $E3 ; BASIC line number:  $E2=2018 $E3=2019 etc
     !byte $07, $9E
     !byte '0' + entry % 10000 / 1000
     !byte '0' + entry %  1000 /  100
     !byte '0' + entry %   100 /   10
     !byte '0' + entry %    10
-    !byte $00, $00, $00           ; end of basic
-
+    !byte $00, $00, $00
 
 ;==========================================================
 ; CODE
 ;==========================================================
 
-entry ;this is a label named 'entry' which can be referred to later on if needed. think of it as a function that can be called.
+; These are labels which can be referred to later on if needed.
+; Think of them as a functions that can be called.
+entry
+    ;; ldy = load y
+    ;
+    ; Load $0b into register Y
+    ; $0b is hex for the number 11
+    ; "hello world!" has 12 characters, and since arrays are 0 indexed,
+    ; the last character has index 11
+    ;
+    ; Register Y will serve as the index register
+    ldy #$0b
 
-    lda #$00                ; load 0 into register a
-    ldy #$0d                ; load $0b (as "hello world!" has 12 characters) into register y (register y will serve as index register, if you're not sure what this means, check out the README file in this folder)
+character_loop
+    ;; lda = load a
+    ;
+    ; Labels, underneath, point to a memory address of where code lives
+    ; We are setting register a to the memory address of print_str + y
+    ; The memory address of print_str + y points to the ASCII code of
+    ; the character at index y
+    ;
+    ; Load a character from print_str at an index
+    ; Our print_str string is "hello world!"
+    ; The current index is in Register Y
+    ; Register Y starts out as 12
+    lda print_str, y
 
-character_loop ; this is also a label
-    lda hello,y             ; load character number y of the string in label hello (hello world!) eg. when y is 2, load character "e"
-    sta $0400,y             ; save data in register a to memory location $0400,y (= $0400 + y). Eg. when y is 2 (register a has the character "e"), $0400,2 = $0402, we'll load character "e" to memory location $0402
-    dey                     ; decrement y by 1 (if y was 2, it is now 1)
-    bpl character_loop      ; is y positive? if yes, then repeat by jumping back to label character_loop. as y decrements with each cycle (due to instruction in line 35), the cycle wil end when y is less than 0 and it will run the next line
-    rts                     ; exit the program
+    ;; sta = store a
 
-hello
-    !scr "hello world!" ; the label named "hello" contains the string we want to print
+    ; Store data from register A to memory location $0400 + number
+    ; $0400 + y = the data from register A
+    ;
+    ; Eg. when y is 2
+    ; and Register A has ascii code for the character "e"
+    ; $0400, 2 = $0402
+    ;
+    ; We'll load the ASCII code to memory location $0402
+    ; $0400 is the start of where screen RAM is saved in memory
+    ; That's where 6502 looks for things to print on the screen
+
+    ; The screen size is 24 * 40
+    ; 24 rows, 40 columns
+    ; Use this information to place our string below the
+    ; printed startup info on the emulator screen
+    sta $0400 + 16 * 40, y
+
+    ;; dey = decrement y
+    ;
+    ; Decrement Y (our index) by 1
+    ; Eg. if y was 2, it is now 1
+    dey
+
+    ;; bpl = branch on plus
+    ;
+    ; After most instructions that have a value result, the negative flag
+    ; is set
+    ;
+    ; So, since our previous instruction was dey and had a value result, the negative flag
+    ; was set
+    ;
+    ; bpl checks that flag. If y is positive, then it will jump back to the label character_loop
+    ; If not, it will continue the program
+
+    ; As y decrements with each cycle, the cycle will end when y is less than 0
+    bpl character_loop
+
+    ;; rts = return from subroutine
+    ; Exits the program
+    rts
+
+; This label gets called by character_loop
+print_str
+    ; !scr is an assembler directive, not an instruction
+    ; It converts ASCII text to ASCII codes
+    !scr "hello world!"
